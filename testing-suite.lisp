85bc4eef0b6b6c817e11fbb58371ebde63e8b203
anoushkt <52332439+anoushkt@users.noreply.github.com>
2020-04-25 20:49:13 -0400

    Create testing-suite.lisp

---
+++ testing-suite.lisp
@@ -0,0 +1,29 @@
+(setf *set-of-orders* NIL)
+(setf *geo-valid-orders* NIL)
+(setf *support-orders* NIL)
+(setf *move-orders* NIL)
+(setf *support-orders* NIL)
+(setf *hold-orders* NIL)
+(setf *convoy-orders* NIL)
+(setf *final-move-orders* NIL)
+
+;;; nothing happens
+(order-input "France" (list "M" "A" "PAR" "BRE"))
+
+(setf *set-of-orders* NIL)
+(setf *geo-valid-orders* NIL)
+(setf *support-orders* NIL)
+(setf *move-orders* NIL)
+(setf *support-orders* NIL)
+(setf *hold-orders* NIL)
+(setf *convoy-orders* NIL)
+(setf *final-move-orders* NIL)
+
+(order-input "France" (list "M" "A" "PAR" "PIC"))
+
+
+
+
+
+
+
