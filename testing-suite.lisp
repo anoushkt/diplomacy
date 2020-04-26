88c56f465425ee43fc64e0a9c7bc635ab9fa8df3
anoushkt <52332439+anoushkt@users.noreply.github.com>
2020-04-26 00:18:59 -0400

    Update testing-suite.lisp

--- testing-suite.lisp
+++ testing-suite.lisp
@@ -10,6 +10,7 @@
 ;;; nothing happens
 (order-input "France" (list "M" "A" "PAR" "BRE"))
 
+;;; RESET before every set of orders
 (setf *set-of-orders* NIL)
 (setf *geo-valid-orders* NIL)
 (setf *support-orders* NIL)
@@ -19,7 +20,33 @@
 (setf *convoy-orders* NIL)
 (setf *final-move-orders* NIL)
 
+;;; TEST 1
+
+;;; SET 1 
 (order-input "France" (list "M" "A" "PAR" "PIC"))
+(order-input "Austria" (list "M" "A" "VIE" "TYR"))
+
+;;; SET 2
+(order-input "France" (list "M" "A" "PIC" "BEL"))
+
+;;; SET 3 
+(order-input "France" (list "M" "A" "BEL" "RUH"))
+
+;;; SET 4
+(order-input "France" (list "M" "A" "RUH"  "MUN")) ;;; FAILS
+
+;;; SET 5
+(order-input "France" (list "M" "A" "RUH"  "MUN"))
+(order-input "Austria" (list "S" "A" "M" "TYR" "RUH" "MUN"))
+
+;;; TEST 2
+;;; SET 1-5 same as TEST 1
+
+;;; SET 6
+
+
+
+
 
 
 
